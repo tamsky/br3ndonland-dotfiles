2a32c142d3f878fd8ce848228dffc4aebf0caf81
Marc Tamsky <tamsky@users.noreply.github.com>
2023-12-19 19:57:19 -0800

    remove more vscode

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -50,7 +50,7 @@ symlink_repo_dotfiles() {
 #   done
 # }
 
-if symlink_repo_dotfiles && symlink_vscode_settings; then
+if symlink_repo_dotfiles; then  # (removed ''&& symlink_vscode_settings')
   echo "-> Symlinking successful. Finishing up..."
   chmod 700 "$HOME"/.gnupg
   chmod 600 "$HOME"/.gnupg/gpg.conf
