f701736d903ae88e2b4acd384b765a84772244be
Marc Tamsky <tamsky@users.noreply.github.com>
2024-05-05 14:44:56 -0700

    add bin/prompt_lite

---
+++ bin/prompt_lite.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+[[ ${INSIDE_EMACS} ]] && exit 0
+
+# hardstatus text:
+declare -a STATUS=( $(hg prompt "{status|full}" 2>/dev/null)
+                  )
+[[ "${STATUS[0]}" ]] && [[ "${STATUS[0]}" != "?" ]] && {
+    STATUS_FLAGS+="{= Wb}"  # white on blue -> reverse mode -> blue on white -> inverted CLUT -> dim yellow on black
+    STATUS_FLAGS+=[${STATUS[0]}]
+    STATUS_FLAGS+="{-}"
+    STATUS_FLAGS+="{=dbu} {-}" # trailing space separates from BOOKMARK
+}
+
+#STATUS=$( echo $(echo -n $-) )
+
+
+# send window title text to screen via fork/exec:
+REPOROOT=$(hg showconfig bundle.mainreporoot 2>/dev/null)
+IN_REPO=$?
+REPO=$(basename $REPOROOT 2>/dev/null)
+DIR=$(basename $PWD 2>/dev/null)
+DIRPARENT=$(dirname $(dirname "$PWD") 2>/dev/null)
+
+BOOKMARK=$(hg summary 2>/dev/null | gawk '/^bookmarks: / { $1="" ;  # delete "bookmarks:" string
+                              gsub("^ ","",$0);        # delete leading spaces
+                              # print warnings
+                              if (NF>1) { multiple_bookmarks=1 ; printf "{! rc}MULTIPLE{-} BOOKMARKS " }
+                              if (index($0,"*")==0) { no_default_bookmark=1 ; printf "{! rc}NO ACTIVE{-} BOOKMARK " }
+                              switch (multiple_bookmarks + no_default_bookmark) {
+                                        # gsub = put each bookmark in (parens)
+                                                  # )
+                                                  # |  {highlight stop}
+                                                  # |  |  space
+                                                  # |  |  |  {highlight start}
+                                                  # |  |  |  |     (
+                                                  # |  |  |  |     |
+                                 case 2 : gsub(" ","){-} {! rc}(",$0) ; printf "{! rc}(%s){-}", $0 ; exit 0
+                                 case 1 : gsub(" ","){-} {! bw}(",$0) ; printf "{! bw}(%s){-}", $0 ; exit 0
+                                 case 0 : printf "{= Wm}%s{-}", $0 ; exit 0
+                              } ;
+                            }'
+           )
+[[ $IN_REPO -eq 0 ]] && {
+    # we are in a hg repo, if no bookmark set, warn us:
+    [[ $BOOKMARK ]] || BOOKMARK=$"<"$"<""NO BOOKMARK"$">"$">" ;
+    BOOKMARK_NAME=$(head -1 $REPOROOT/.hg/bookmarks.current 2>/dev/null)
+}
+
+# =~ ^[0-9a-zA-Z_]
+#     ! [[ ${_last_history_number_had_new_command} =~ ^[0-9a-zA-Z] ]] && {
+
+# set the screen title in the background
+[[ $WINDOW ]] && ( ( screen -p $WINDOW -X title "${REPO}" ) & )
+
+
+# we need to pick a path to wakatime
+WAKATIME_BIN=$(type -p wakatime || type -p wakatime-cli)
+
+# call wakatime in the background
+[[ ${_last_history_number_had_new_command} ]] &&
+    $(type -p "${_last_history_number_had_new_command}">/dev/null) && {
+        ## we only log if the string is an executable file or function name
+        SHORT_PWD=$(echo $PWD | sed -e 's@'$HOME'@@' | tr '/' '^' )
+        echo $(date) ${REPO:-$SHORT_PWD} ${BOOKMARK_NAME} ${_last_history_number_had_new_command} >> ~/.wakatime.prompt_lite.log ;
+        ( ( ${WAKATIME_BIN} --write --plugin "bash-wakatime/0.1.1" \
+                     --entity-type app \
+                     --project "${REPO:-$SHORT_PWD}" \
+                     --alternate-project "${BOOKMARK_NAME}" \
+                     --entity "${_last_history_number_had_new_command}" 2>&1 >/dev/null ) & ) ;
+    }
+
+# emit to SCREEN(1) as a hardstatus string
+#
+#                  --> [AMR!?]-hg flags         --> hg bookmark list
+#                  |                            |
+#                  ~~~~~~~~~~~~                 ~~~~~~~~~~~
+#echo -n $'\033']0\;${STATUS[0]}"{-} {= wdk}(${BOOKMARK}){-}"$'\007'
+
+echo -n $'\033'"]0;${STATUS_FLAGS}${BOOKMARK}"$'\007'
