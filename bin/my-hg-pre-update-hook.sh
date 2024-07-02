c634216641890e953d9e45989a9c189a80a47a75
Marc Tamsky <tamsky@users.noreply.github.com>
2024-07-01 16:23:20 -1000

    shfmt: style cleanup only

--- bin/my-hg-pre-update-hook.sh
+++ bin/my-hg-pre-update-hook.sh
@@ -3,14 +3,15 @@
 
 # handle simple update ( PARENT1 set, PARENT2 nil)
 [[ $1 ]] && [[ -z $2 ]] && {
-  # echo "update detected" ;
-  exit 0 ; }
+    # echo "update detected" ;
+    exit 0
+}
 
 # handle merge ( PARENT1 set (unchanged), PARENT2 set (working directory ID) )
 [[ $1 ]] && [[ $2 ]] && {
-  echo "hg pre-update hook triggered: PARENT1=$1, PARENT2=$2" ;
-  hg diff -r $2 -r $1 | grep ^+ | grep 'DO NOT MERGE' 2>&1 >/dev/null &&
-    exit 1 || exit 0 ;
+    echo "hg pre-update hook triggered: PARENT1=$1, PARENT2=$2"
+    hg diff -r $2 -r $1 | grep ^+ | grep 'DO NOT MERGE' 2>&1 >/dev/null &&
+        exit 1 || exit 0
 }
 
 [[ -z $1 ]] && echo "unknown state, PARENT1 unset" && exit 1
