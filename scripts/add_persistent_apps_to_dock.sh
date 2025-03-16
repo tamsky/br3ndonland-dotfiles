1647244092da6c8ab566499fa1e8527168964a45
Marc Tamsky <tamsky@users.noreply.github.com>
2025-03-16 09:21:54 -0700

    found an add_persistent_apps_to_dock.sh script

---
+++ scripts/add_persistent_apps_to_dock.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+#   file:
+#       add_persistent_apps_to_dock.sh
+#
+#   function:
+#       add specified application bundle(s) to Dock as persistent application(s)
+#
+#   usage:
+#       ./add_persistent_apps_to_dock.sh file [file ...]
+#
+#           file : absolute posix path of application bundle
+#
+#   version:
+#       0.10
+#
+#   source:
+#       https://discussions.apple.com/thread/7538664
+#
+#   author: Hiroto
+#
+shopt -s extglob
+for f in "$@"
+do
+    f=${f/%+(\/)}/                              # normalise bundle path so that it ends with /
+    fq=$(sed 's/[^[:alnum:]]/\\&/g' <<< "$f")   # quote meta characters for regex
+
+    [[ -n $(defaults read com.apple.dock persistent-apps |
+        sed -En "s/\"_CFURLString\" = \"?($fq)\"?;/\1/p" ) ]] && continue   # already exists
+
+    defaults write com.apple.dock persistent-apps -array-add "
+<dict>
+    <key>tile-data</key>
+    <dict>
+        <key>file-data</key>
+        <dict>
+            <key>_CFURLString</key>
+            <string>${f}</string>
+            <key>_CFURLStringType</key>
+            <integer>0</integer>
+        </dict>
+    </dict>
+</dict>
+"
+done
+killall Dock
