df9bb345ce15d58b3e5a1f9ef90cfd40a2e463a0
Marc Tamsky <tamsky@users.noreply.github.com>
2025-03-20 00:47:49 -0700

    +shfmt

--- scripts/add_persistent_apps_to_dock.sh
+++ scripts/add_persistent_apps_to_dock.sh
@@ -20,18 +20,17 @@
 #   author: Hiroto
 #
 shopt -s extglob
-for f in "$@"
-do
-    f=${f/%+(\/)}/                              # normalise bundle path so that it ends with /
-    fq=$(sed 's/[^[:alnum:]]/\\&/g' <<< "$f")   # quote meta characters for regex
+for f in "$@"; do
+  f=${f/%+(\/)}/                              # normalise bundle path so that it ends with /
+  fq=$(sed 's/[^[:alnum:]]/\\&/g' <<< "$f")   # quote meta characters for regex
 
-    CFURLStringType=0
-    egrep -q '^file://' <<< "${f}"  && CFURLStringType=15
+  CFURLStringType=0
+  egrep -q '^file://' <<< "${f}"  && CFURLStringType=15
 
-    [[ -n $(defaults read com.apple.dock persistent-apps |
-        sed -En "s/\"_CFURLString\" = \"?($fq)\"?;/\1/p" ) ]] && continue   # already exists
+  [[ -n $(defaults read com.apple.dock persistent-apps |
+      sed -En "s/\"_CFURLString\" = \"?($fq)\"?;/\1/p" ) ]] && continue   # already exists
 
-    defaults write com.apple.dock persistent-apps -array-add "
+  defaults write com.apple.dock persistent-apps -array-add "
 <dict>
     <key>tile-data</key>
     <dict>
